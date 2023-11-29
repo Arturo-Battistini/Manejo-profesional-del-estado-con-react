5a8fb876ff27c7aa4d5dd4963e3de3e71ec62aa8
Arturo Battistini <aabattistini@gmail..com>
2023-11-29 13:09:33 -0300

    segundo commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'Manejo-profesional-del-estado-con-react'
+  base:'estados_en_react/'
 })
